22ae5a29bd96331880ad0bff704ad34aba7103f8
Shinwoo PARK <devpysweb@gmail.com>
2024-06-13 17:54:33 +0000

    feat: add configurations

---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
+import mdx from "@mdx-js/rollup";
+import { resolve } from "node:path";
+import remarkGfm from "remark-gfm";
+import withSlug from "rehype-slug";
+import withToc from "@stefanprobst/rehype-extract-toc";
+import withTocExport from "@stefanprobst/rehype-extract-toc/mdx";
+import dynamicImport from "vite-plugin-dynamic-import";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    mdx({
+      rehypePlugins: [withSlug, withToc, withTocExport],
+      remarkPlugins: [remarkGfm],
+    }),
+    dynamicImport(),
+  ],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
+  resolve: {
+    alias: {
+      "@components": resolve(__dirname, "./components"),
+      "@": resolve(__dirname, "./src"),
+      "@pswui-lib": resolve(__dirname, "./lib.tsx"),
+    },
+  },
+});
