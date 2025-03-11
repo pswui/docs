1281e89275346a0e3bafdb1e5ddb5940652c3588
Shinwoo PARK <shinwoo.park@psw.kr>
2025-03-11 20:35:31 +0900

    fix: use @tailwindcss/vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { resolve } from "node:path";
 import mdx from "@mdx-js/rollup";
 import withToc from "@stefanprobst/rehype-extract-toc";
 import withTocExport from "@stefanprobst/rehype-extract-toc/mdx";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import withSlug from "rehype-slug";
 import remarkGfm from "remark-gfm";
-import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
 import dynamicImport from "vite-plugin-dynamic-import";
 
@@ -13,17 +13,13 @@ import dynamicImport from "vite-plugin-dynamic-import";
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     mdx({
       rehypePlugins: [withSlug, withToc, withTocExport],
       remarkPlugins: [remarkGfm],
     }),
     dynamicImport(),
   ],
-  css: {
-    postcss: {
-      plugins: [tailwindcss()],
-    },
-  },
   resolve: {
     alias: {
       "@pswui": resolve(__dirname, "./src/pswui/components"),
